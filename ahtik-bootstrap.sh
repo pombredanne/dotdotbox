9982c33e6390bf8f734665a91a2f3e121f9d247b
Ahti Kitsik <ahti@codehoop.com>
2014-03-23 10:51:39 +0200

    improved docs

--- ahtik-bootstrap.sh
+++ ahtik-bootstrap.sh
@@ -6,8 +6,8 @@
 
 # UNITY
 # fix expose
-sudo add-apt-repository ppa:mc3man/fixing-scale
-https://launchpad.net/ubuntu-tweak/0.8.x/0.8.5/+download/ubuntu-tweak_0.8.5-1_all.deb
+# sudo add-apt-repository ppa:mc3man/fixing-scale
+# https://launchpad.net/ubuntu-tweak/0.8.x/0.8.5/+download/ubuntu-tweak_0.8.5-1_all.deb
 
 gsettings set org.gnome.desktop.interface menubar-accel ""
 
@@ -19,10 +19,10 @@ gconftool --type boolean --set /apps/nautilus/preferences/show_desktop false
 # Starting openvpn
 
 #OPENVPN
-sudo openvpn --config /home/ahtik/vpn/xxx.ovpn&
+sudo openvpn --config /home/userhome/vpn/xxx.ovpn&
 
 
-# XMONAD
+# XMONAD (Unity is in focus for now, if fails to comply, will switch back to XMONAD!)
 # Run to update haskell libs
 cabal install cabal-install
 tint2 xcompmgr docky obconf synapse thunar thunar-archive-plugin thunar-media-tags-plugin nitrogen parcellite
@@ -47,9 +47,11 @@ myusername ALL=(ALL) NOPASSWD: ALL
 # ZSH
 chsh -s /bin/zsh
 
-
-# INSTALL PACKAGES
+# SKYPE (must be separate role)
 download skype from http://www.skype.com/en/download-skype/skype-for-linux/downloading/?type=ubuntu64
+
+
+# LASTPASS (must be separate role as most users wont need it)
 install lastpass from https://lastpass.com/misc_download.php
 
 
@@ -59,7 +61,7 @@ ttf-mscorefonts
 wget http://webupd8.googlecode.com/files/install-google-fonts
 chmod +x install-google-fonts
 
-# Install SourceCodePro fonts
+# Install SourceCodePro fonts (Hi Prio)
 #!/bin/bash
 FONT_NAME="SourceCodePro"
 URL="http://sourceforge.net/projects/sourcecodepro.adobe/files/latest/download"
@@ -72,18 +74,18 @@ cp *.otf ~/.fonts
 fc-cache -f -v
 
 
-# Estonian ID card support
+# Estonian ID card support (not sure if this still works, must be tested!!)
 sudo apt-get install esteid
 sudo add-apt-repository https://installer.id.ee/media/ubuntu/
 
-
+# Comment from the web how to get the estid working if fails with the latest. But this is for 12.04 and we need to support 14.04LTS!
 sudo apt-get remove qdigidoc mozilla-esteid libdigidocpp0 thunderbird-esteid libdigidoc libdigidoc-dev libdigidocpp libdigidocpp-dev libdigidocpp
 sudo dpkg -r libdigidoc2
 sudo dpkg -r libdigidoc-common
 rm -rf /var/cache/apt/archives/libdigidoc2_3.7.0.909-ubuntu-12-04_i386.deb rm -rf /var/cache/apt/archives/libdigidocpp0_3.7.0.969-ubuntu-12-04_i386.deb
 
 
-# Latest java (Not needec for now as different installation is used??)
+# Latest java (Not neede for now as different installation is used??) TODO: Check the current installation method to choose the best (bonus points if java 8 can be installed in parallel!)
 sudo add-apt-repository ppa:webupd8team/java
 #Java
 sudo apt-get install oracle-java6-installer
@@ -102,7 +104,7 @@ update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-7-oracle/bi
 # deb-src http://ppa.launchpad.net/esteid/ppa/ubuntu precise main
 
 
-# Update kernel
+# Update kernel (possibly not needed with 14.04LTS! Upgrade reasons were to get better docker and expresscard support.
 wget -c kernel.ubuntu.com/~kernel-ppa/mainline/v3.10-saucy/linux-headers-3.10.0-031000_3.10.0-031000.201306301935_all.deb
 wget -c kernel.ubuntu.com/~kernel-ppa/mainline/v3.10-saucy/linux-headers-3.10.0-031000-generic_3.10.0-031000.201306301935_amd64.deb
 wget -c kernel.ubuntu.com/~kernel-ppa/mainline/v3.10-saucy/linux-image-3.10.0-031000-generic_3.10.0-031000.201306301935_amd64.deb
@@ -114,31 +116,35 @@ sudo dpkg -i *.deb
 sudo dpkg -i /opt/cache/kernel-3.11/*.deb
 
 
+## OPENBOX section (openbox is off the table for now)
+# sudo apt-get install openbox
 
-## OPENBOX section
-sudo apt-get install openbox
 
-
+# xfce is nice but a little less developed than, let's say unity.. so if the performance and productivity can be maintained with unity... or the fallback to xmonad..
 # sudo add-apt-repository ppa:xubuntu-dev/xfce-4.10
 # sudo add-apt-repository ppa:xubuntu-dev/xfce-4.12
 
 
-#rhythmbox update to fix the memory leak
+#rhythmbox update to fix the memory leak (TODO: Still relevant?)
 sudo add-apt-repository ppa:edgar2705/my-ppa
 sudo add-apt-repository ppa:webupd8team/rhythmbox
 
 
-sudo add-apt-repository ppa:ubuntu-x-swat/x-updates
-sudo ppa-purge ppa:ubuntu-x-swat/x-updates
+# Too risky for that kind of public playbook we try to build here..
+# sudo add-apt-repository ppa:ubuntu-x-swat/x-updates
+# sudo ppa-purge ppa:ubuntu-x-swat/x-updates
 
-sudo apt-get install xfce4-utils xfce4-cpugraph-plugin xfce4-datetime-plugin
-xfce4-dict xfce4-indicator-plugin xfce4-netload-plugin xfce4-power-manager
-xfce4-quicklauncher-plugin xfce4-screenshooter xfce4-systemload-plugin moreutils
+# XFCE off the table for now.. unless someone contributes a good ansible role for xfce4?
 
-estonianidcard
+# sudo apt-get install xfce4-utils xfce4-cpugraph-plugin xfce4-datetime-plugin
+# xfce4-dict xfce4-indicator-plugin xfce4-netload-plugin xfce4-power-manager
+# xfce4-quicklauncher-plugin xfce4-screenshooter xfce4-systemload-plugin moreutils
 
-#install latest from https://01.org/linuxgraphics/downloads
 
+#install latest from
+# https://01.org/linuxgraphics/downloads
+
+# MOSH
 # sudo apt-get install python-software-properties
 # sudo add-apt-repository ppa:keithw/mosh
 # sudo apt-get update
